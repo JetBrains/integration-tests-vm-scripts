a0c955016b13349199fa45a79f6beede3bcd510b
Ivan Shakhov <Ivan.Shakhov@jetbrains.com>
2016-08-31 10:49:06 +0200

    mod IntegrationTests.ps1 to support Array of ExeTORunForTests

--- Platform/build/TestProduct/Impl/IntegrationTests.ps1
+++ Platform/build/TestProduct/Impl/IntegrationTests.ps1
@@ -3,7 +3,7 @@
     [Parameter(Position=0)]$NUnitCpu = $null, # Inherit from current runtime by default
     [Parameter(Position=0)]$NUnitRuntime = $null, # Inherit from current runtime by default
     
-    [Parameter(Position=0, Mandatory=$true)][String[]]$FilesToTest, # "path_to_dll1", "path_to_dll2"
+    [Parameter(Position=0, Mandatory=$true)][System.Collections.ArrayList]$FilesToTest,  #InTestVSVersionMajor, ExeToRunForTests, fileToTest
     [Parameter(Position=0)]$CountOfMachinesToStart = 1,
     [Parameter(Position=0, Mandatory=$true)]$cloneNamePattern,
     [Parameter(Position=0, Mandatory=$true)]$VmName,
@@ -23,7 +23,7 @@ function Run
     & "$ProductHomeDir\Platform\build\TestProduct\Impl\IntegrationTests.PowerOff.ps1" -cloneNamePattern $cloneNamePattern -ViServerData $ViServerData
 
     Try {
-        & "$ProductHomeDir\Platform\build\TestProduct\Impl\IntegrationTests.PowerOnRun.ps1" -FilesToTest $FilesToTest `
+        & "$ProductHomeDir\Platform\build\TestProduct\Impl\IntegrationTests.PowerOnRun2.ps1" -FilesToTest $FilesToTest `
             -cloneNamePattern $cloneNamePattern -VmName $VmName -CountOfMachinesToStart $CountOfMachinesToStart -NUnitExcludeCategory $NUnitExcludeCategory -NUnitIncludeCategory $NUnitIncludeCategory `
             -NUnitCpu $NUnitCpu -NUnitRuntime $NUnitRuntime `
             -ViServerData $ViServerData -GuestCredentials $GuestCredentials
