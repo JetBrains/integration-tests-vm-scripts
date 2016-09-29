f48166d3bbdc18d0b89785c365ac98f9780f19b5
Ivan Shakhov <Ivan.Shakhov@jetbrains.com>
2016-09-29 22:01:46 +0200

    Revert "delete clones if there are less then 100Gb free on datastore"
    This reverts commit 10ea50f44e252eb206ea7f6ab5f11b93741b6f76.

--- Platform/build/TestProduct/Impl/InTest/CloneStartVM.ps1
+++ Platform/build/TestProduct/Impl/InTest/CloneStartVM.ps1
@@ -40,17 +40,7 @@ function Clone()
 
     $sourceVM =  $vmHost | get-vm -Name $name
     $datastore = $sourceVM | get-datastore
-
-    #free space
-    $freespaceGb =0
-    while ($freespaceGb -le 100) {
-      $freespaceGb = ([Math]::Round(($datastore.ExtensionData.Summary.FreeSpace)/1GB,0))
-      Write-Host TargetDataStore: $datastore, FreeSpaceGB: $freespaceGb Gb
-      $cloneVmToDelete = @($datastore | Get-VM -Name  "*_clone_*" | where {$_.PowerState -eq "PoweredOff"} | Select -First 1)
-      if ($cloneVmToDelete.Count -eq 0) {break}
-      Write-Host Delete vm $cloneVmToDelete.Name
-      Remove-VM -VM $cloneVmToDelete -DeleteFromDisk:$true -Confirm:$false -RunAsync:$false
-    }
+    Write-Host TargetDataStore: $datastore, FreeSpaceGB: ([Math]::Round(($datastore.ExtensionData.Summary.FreeSpace)/1GB,0))Gb
     
     $sourceVMView = $sourceVM | Get-View
     $cloneFolder = $sourceVMView.Parent
