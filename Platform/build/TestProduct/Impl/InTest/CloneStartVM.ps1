d0ce2acd594b44aba891df07c462a72e79636689
Ivan Shakhov <Ivan.Shakhov@jetbrains.com>
2016-12-29 13:35:04 +0100

    Revert "fixed CpuLimitMhz"
    This reverts commit 6f73faae0463e98383d35c090c76dd28b8116078.

--- Platform/build/TestProduct/Impl/InTest/CloneStartVM.ps1
+++ Platform/build/TestProduct/Impl/InTest/CloneStartVM.ps1
@@ -62,10 +62,7 @@ function Clone()
 
     $targetVM =  $vmHost | get-vm -Name $cloneName
     $targetVM | Get-FloppyDrive | Remove-FloppyDrive -Confirm:$false
-    
-    if ($sourceVM.VMResourceConfiguration.CpuLimitMhz > 0) {
-      $targetVM | Get-VMResourceConfiguration | Set-VMResourceConfiguration -CPULimitMhz $sourceVM.VMResourceConfiguration.CpuLimitMhz
-    }
+    #$targetVM | Get-VMResourceConfiguration | Set-VMResourceConfiguration -CPULimitMhz $sourceVM.VMResourceConfiguration.CpuLimitMhz
 
     Write-Host 't='$t
     Write-Host 'cloneName'$cloneName
