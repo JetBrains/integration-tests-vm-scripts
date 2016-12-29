6f73faae0463e98383d35c090c76dd28b8116078
Ivan Shakhov <Ivan.Shakhov@jetbrains.com>
2016-12-29 11:22:06 +0100

    fixed CpuLimitMhz

--- Platform/build/TestProduct/Impl/InTest/CloneStartVM.ps1
+++ Platform/build/TestProduct/Impl/InTest/CloneStartVM.ps1
@@ -62,7 +62,10 @@ function Clone()
 
     $targetVM =  $vmHost | get-vm -Name $cloneName
     $targetVM | Get-FloppyDrive | Remove-FloppyDrive -Confirm:$false
-    #$targetVM | Get-VMResourceConfiguration | Set-VMResourceConfiguration -CPULimitMhz $sourceVM.VMResourceConfiguration.CpuLimitMhz
+    
+    if ($sourceVM.VMResourceConfiguration.CpuLimitMhz > 0) {
+      $targetVM | Get-VMResourceConfiguration | Set-VMResourceConfiguration -CPULimitMhz $sourceVM.VMResourceConfiguration.CpuLimitMhz
+    }
 
     Write-Host 't='$t
     Write-Host 'cloneName'$cloneName
