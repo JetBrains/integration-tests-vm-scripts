915d0d2105d5a37aba22b9f29298ce27d2534a92
Ivan Shakhov <Ivan.Shakhov@jetbrains.com>
2016-12-29 09:43:47 +0100

    copy cpu limit from source vm to target vm

--- Platform/build/TestProduct/Impl/InTest/CloneStartVM.ps1
+++ Platform/build/TestProduct/Impl/InTest/CloneStartVM.ps1
@@ -60,6 +60,10 @@ function Clone()
  
     $t = $sourceVMView.CloneVM( $cloneFolder, $cloneName, $cloneSpec ) #  requires VCenter
 
+    $targetVM =  $vmHost | get-vm -Name $cloneName
+    $targetVM | Get-FloppyDrive | Remove-FloppyDrive -Confirm:$false
+    $targetVM | Get-VMResourceConfiguration | Set-VMResourceConfiguration -CPULimitMhz $sourceVM.VMResourceConfiguration.CpuLimitMhz
+
     Write-Host 't='$t
     Write-Host 'cloneName'$cloneName
 }
