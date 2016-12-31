7fb0683cc9a7a707b3e887c98e47fa8bd2db87db
Ivan Shakhov <Ivan.Shakhov@jetbrains.com>
2016-12-31 17:31:24 +0100

    fix call ViServer.Connect

--- Platform/build/TestProduct/Impl/IntegrationTests.PowerOnRun2.ps1
+++ Platform/build/TestProduct/Impl/IntegrationTests.PowerOnRun2.ps1
@@ -17,7 +17,6 @@
 <#ScriptPrologue#> Set-StrictMode -Version Latest; $ErrorActionPreference = [System.Management.Automation.ActionPreference]::Stop
 function GetDirectoryNameOfFileAbove($markerfile) { $result = ""; $path = $MyInvocation.ScriptName; while(($path -ne "") -and ($path -ne $null) -and ($result -eq "")) { if(Test-Path $(Join-Path $path $markerfile)) {$result=$path}; $path = Split-Path $path }; if($result -eq ""){throw "Could not find marker file $markerfile in parent folders."} return $result; }
 $ProductHomeDir = GetDirectoryNameOfFileAbove "Product.Root"
-function Get-ScriptDirectory { Split-Path $script:MyInvocation.MyCommand.Path }
 
 function MakeScriptBlock($machine, $fileToTest)
 {
@@ -120,7 +119,7 @@ function TestsInMachines($machines, $FilesToTest)
 
 function FreeSpace()
 {
-    & (Join-Path (Get-ScriptDirectory) "ViServer.Connect.ps1") -ViServerAddress $ViServerData[0] -ViServerLogin $ViServerData[1] -ViServerPasword $ViServerData[2] | Out-Null
+    & "$ProductHomeDir\Platform\build\TestProduct\Impl\InTest\ViServer.Connect.ps1" -ViServerAddress $ViServerData[0] -ViServerLogin $ViServerData[1] -ViServerPasword $ViServerData[2] | Out-Null
 
     $vmHost = get-vmhost
 
